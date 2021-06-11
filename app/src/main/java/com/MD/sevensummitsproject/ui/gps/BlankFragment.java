b66cfaa555ce2b8316b4e0da84a93878837e80df
scherzm1 <manuelschz>
2021-06-11 13:58:19 +0200

    Grundlagen der GPS Einstellungen

---
+++ app/src/main/java/com/MD/sevensummitsproject/ui/gps/BlankFragment.java
@@ -0,0 +1,40 @@
+package com.MD.sevensummitsproject.ui.gps;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.MD.sevensummitsproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main2, container, false);
+    }
+}
